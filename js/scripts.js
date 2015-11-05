88d3c3a20bf9b1ed98a451baaa52f5d1c1d796f0
ncoletto <ncoletto@gmail.com>
2015-11-05 00:26:14 -0300

    loginApp2
    login acelerometro sms paneles swipe

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,50 @@
-$( document ).on( "pageinit", "#tiempo,#mapa", function() {
-    $( document ).on( "swipeleft swiperight", "#tiempo,#mapa", function( e ) {
+$( document ).on( "pageinit", "#mapa", function() {
+    $( document ).on( "swipeleft swiperight", "#mapa", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
         if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
             if ( e.type === "swipeleft"  ) {
-                $( "#right-panel" ).panel( "open" );
+                $( "#right-panel1" ).panel( "open" );
             } else if ( e.type === "swiperight" ) {
+                $( "#left-panel1" ).panel( "open" );
+            }
+        }
+    });
+});
+
+
+
+$( document ).on( "pageinit", "#tiempo", function() {
+    $( document ).on( "swipeleft swiperight", "#tiempo", function( f ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            if ( f.type === "swipeleft"  ) {
+                $( "#right-panel" ).panel( "open" );
+            } else if ( f.type === "swiperight" ) {
                 $( "#left-panel" ).panel( "open" );
             }
         }
     });
 });
 
+
+
+$( document ).on( "pageinit", "#bluetooth", function() {
+    $( document ).on( "swipeleft swiperight", "#bluetooth", function( f ) {
+        // We check if there is no open panel on the page because otherwise
+        // a swipe to close the left panel would also open the right panel (and v.v.).
+        // We do this by checking the data that the framework stores on the page element (panel: open).
+        if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+            if ( f.type === "swipeleft"  ) {
+                $( "#right-panel2" ).panel( "open" );
+            } else if ( f.type === "swiperight" ) {
+                $( "#left-panel2" ).panel( "open" );
+            }
+        }
+    });
+});
 
 
